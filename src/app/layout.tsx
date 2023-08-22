6bf92ef050efa5353790a33538e0168282563dae
Engineer Mohammed <eng_abo_hussien@yahoo.com>
2023-08-22 22:36:38 +0300

    add slides

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import HeaderTop from "@/components/HeaderTop";
 import "./globals.css";
-import { Urbanist } from "next/font/google";
+import { Inter } from "next/font/google";
 import HeaderMain from "@/components/HeaderMain";
 import Navbar from "@/components/Navbar";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const font = Urbanist({ subsets: ["latin"] });
+const font = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Farida Store",
   description: "The Market of Farida Store",
